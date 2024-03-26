1cb7d19c1eb8a282ac189c2c4e957bfdd06a8eca
pranjal singh sengar <67635283+pranjalsinghsengar@users.noreply.github.com>
2024-03-26 14:24:55 +0530

    backend -u

--- src/api/index.js
+++ src/api/index.js
@@ -37,6 +37,14 @@ const User = require('./models/users');
 
 // end points for ragistration of the user
 
+const createToken = userId => {
+  const payload = {
+    userId: userId,
+  };
+  const token = jwt.sign(payload, 'Q$r2K6W8n!jCW%Zk', {expireIn: '1h'});
+  return token;
+};
+
 app.post('/register', async (req, res) => {
   const {name, email, password, image} = req.body;
 
@@ -55,11 +63,39 @@ app.post('/register', async (req, res) => {
     .then(() => {
       res.status(200).json({
         massage: 'user registered ssdfuccessfully',
-        user: { name, email, password, image},
+        user: {name, email, password, image},
       });
     })
     .catch(err => {
       console.log('err in ragister user', err);
       res.status(500).json({massage: 'error in ragister user'});
+    });
+});
+
+app.post('/login', (req, res) => {
+  const {email, password} = req.body;
+
+  if (!email || !password) {
+    return res.status(404).json({message: 'email and password are required'});
+  }
+
+  User.findOne({email})
+    .then(user => {
+      if (!user) {
+        // if user not found
+        return res.status(404).json({message: 'user not found'});
+      }
+
+      // compare
+      if (user.password !== password) {
+        return res.status(404).json({message: 'invalid password'});
+      }
+
+      const token = createToken(user._id);
+      res.status(200).json({token: token});
+    })
+    .catch(err => {
+      console.log('err in finding user', err);
+      res.status(500).json({message: 'internal server error!'});
     });
 });
